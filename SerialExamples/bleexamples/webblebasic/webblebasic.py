ecfaa55c3b4835f971c0f791089f3133417946a2
Zevensuy Rodriguez <zevenrodriguez@users.noreply.github.com>
2026-03-18 20:23:41 +0000

    all examples done

--- SerialExamples/bleexamples/webblebasic/webblebasic.py
+++ SerialExamples/bleexamples/webblebasic/webblebasic.py
@@ -1,7 +1,8 @@
 import time
 import board
 import digitalio
-import analogio
+from adafruit_apds9960.apds9960 import APDS9960
+
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
@@ -9,10 +10,23 @@ from adafruit_ble.services.nordic import UARTService
 ble  = BLERadio()
 uart = UARTService()           # handles NUS UUIDs automatically
 adv  = ProvideServicesAdvertisement(uart)
+print(uart.uuid)
+
 
 led    = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
-sensor = analogio.AnalogIn(board.A0)
+
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+apds9960 = APDS9960(i2c)
+
+apds9960.enable_proximity = True
+apds9960.proximity_gain = 2
+
+currentValue = 0
+proximity_readings = []
+num_readings = 10  # Number of readings to average
+
 
 while True:
     print("Advertising...")
@@ -33,8 +47,21 @@ while True:
                 led.value = False
 
         # Send sensor value as a simple string
-        raw = sensor.value >> 8          # scale 0–65535 → 0–255
-        uart.write(f"{raw}\n".encode())
-        time.sleep(0.1)
+        proximity = apds9960.proximity
+        proximity_readings.append(proximity)
+    
+        # Keep only the last num_readings
+        if len(proximity_readings) > num_readings:
+            proximity_readings.pop(0)
+        
+        # Calculate average
+        average_proximity = sum(proximity_readings) / len(proximity_readings)
+        
+        if currentValue != average_proximity:
+            print(f"Average Proximity: {average_proximity}")
+            currentValue = average_proximity
+            currentValue = int(currentValue)
+            uart.write(f"{currentValue}\n".encode())
+            time.sleep(0.1)
 
     print("Disconnected.")
