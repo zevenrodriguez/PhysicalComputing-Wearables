e587e3ababb0ea26fd1fee358de710b3664cdf94
Zevensuy Rodriguez <zevenrodriguez@users.noreply.github.com>
2026-09-10 09:08:17 -0400

    sound files

--- SerialExamples/bleexamples/bletounitypython/BLEBridge.py
+++ SerialExamples/bleexamples/bletounitypython/BLEBridge.py
@@ -9,7 +9,6 @@ from tkinter import scrolledtext
 from datetime import datetime
 from bleak import BleakScanner, BleakClient
 
-# top of ble_bridge_ui.py — update these three lines
 DEFAULT_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
 DEFAULT_TX      = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 DEFAULT_RX      = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
