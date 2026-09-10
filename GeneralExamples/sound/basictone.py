25ecb5300d1c92633c760bf553d747f1ada8d22a
Zevensuy Rodriguez <zevenrodriguez@users.noreply.github.com>
2026-09-10 09:22:00 -0400

    Refactor tone playback logic with melody support

--- GeneralExamples/sound/basictone.py
+++ GeneralExamples/sound/basictone.py
@@ -6,12 +6,22 @@ import pwmio
 # For the M0 boards:
 piezo = pwmio.PWMOut(board.A2, duty_cycle=0, frequency=440, variable_frequency=True)
 
+melody = (262, 294, 330, 349, 392, 440, 494, 523)
+note_index = 0
+start_time = time.monotonic()
+interval = 0
+playing = False
+
 while True:
-    for f in (262, 294, 330, 349, 392, 440, 494, 523):
-        piezo.frequency = f
-        piezo.duty_cycle = 65535 // 2  # On 50%
-        # number // number integer division
-        time.sleep(0.25)  # On for 1/4 second
-        piezo.duty_cycle = 0  # Off
-        time.sleep(0.05)  # Pause between notes
-    time.sleep(0.5)
+    if time.monotonic() - start_time > interval:
+        playing = not playing
+        if playing:
+            piezo.frequency = melody[note_index]
+            piezo.duty_cycle = 65535 // 2
+            note_index = (note_index + 1) % len(melody)
+            interval = 0.25
+        else:
+            piezo.duty_cycle = 0
+            interval = 0.5 if note_index == 0 else 0.05
+
+        start_time = time.monotonic()
